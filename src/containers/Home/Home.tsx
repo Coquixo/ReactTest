630989eec47b914c6c8836c421a42979a76350ec
Alex Lopez <coquixo98@gmail.com>
2023-08-26 18:46:08 +0200

    add react router dom

---
+++ src/containers/Home/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+	return (
+		<div className="text-red-500">
+			<div>Esto es el Home</div>
+		</div>
+	);
+};
+
+export default Home;
---
+++ src/containers/Test1/Test1.tsx
@@ -0,0 +1,9 @@
+const Test1 = () => {
+	return (
+		<div>
+			<div>Esto es el Test1</div>
+		</div>
+	);
+};
+
+export default Test1;
